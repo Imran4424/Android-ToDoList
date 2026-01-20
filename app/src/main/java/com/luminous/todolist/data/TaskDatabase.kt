209c2b962f226e8658345a38c1ac01fc99aaac33
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-21 00:06:06 +0600

    task database

--- app/src/main/java/com/luminous/todolist/data/TaskDatabase.kt
+++ app/src/main/java/com/luminous/todolist/data/TaskDatabase.kt
@@ -1,9 +1,34 @@
 package com.luminous.todolist.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [TaskEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
         abstract fun taskDao(): TaskDao
+
+        companion object {
+                @Volatile
+                private var INSTANCE: AppDatabase? = null
+
+                fun getInstance(context: Context): AppDatabase {
+                        val tempInstance = INSTANCE
+                        if (tempInstance != null) {
+                                return tempInstance
+                        }
+
+                        synchronized(this) {
+                                val instance = Room.databaseBuilder(
+                                        context.applicationContext,
+                                        AppDatabase::class.java,
+                                        "app_database"
+                                ).build()
+
+                                INSTANCE = instance
+                                return instance
+                        }
+                }
+        }
 }
