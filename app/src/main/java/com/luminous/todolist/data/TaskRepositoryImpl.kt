3ee2ca73fc98a11b4d0221c8fc5bc749f423875e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-21 00:23:14 +0600

    task repository

--- app/src/main/java/com/luminous/todolist/data/TaskRepositoryImpl.kt
+++ app/src/main/java/com/luminous/todolist/data/TaskRepositoryImpl.kt
@@ -1,10 +1,11 @@
 package com.luminous.todolist.data
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class TaskRepositoryImpl(private val taskDao: TaskDao) : TaskRepository {
         override fun observeTasks(completed: Boolean): Flow<List<Task>> {
-                TODO("Not yet implemented")
+                return taskDao.observeByCompletion(completed).map { entities -> entities.map { it.toTask() } }
         }
 
         override suspend fun addTask(title: String) {
@@ -22,4 +23,16 @@ class TaskRepositoryImpl(private val taskDao: TaskDao) : TaskRepository {
         override suspend fun updateTaskTitle(id: Long, title: String) {
                 TODO("Not yet implemented")
         }
+}
+
+
+// Extension function of TaskEntity to convert TaskEntity to Task
+private fun TaskEntity.toTask(): Task {
+        return Task(
+                id = id,
+                title = title,
+                isCompleted = isCompleted,
+                createdAt = createdAt,
+                completedAt = completedAt
+        )
 }
